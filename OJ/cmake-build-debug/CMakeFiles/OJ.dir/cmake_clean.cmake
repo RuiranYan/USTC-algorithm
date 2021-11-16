73b6211b4c7e2721e3da9d75d7d41959bb31cd8f
RuiranYan <624451680@qq.com>
2021-11-16 15:12:45 +0800

    add README.md

--- OJ/cmake-build-debug/CMakeFiles/OJ.dir/cmake_clean.cmake
+++ OJ/cmake-build-debug/CMakeFiles/OJ.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/OJ.dir/E2-3.cpp.obj"
-  "OJ.exe"
-  "OJ.exe.manifest"
+  "CMakeFiles/OJ.dir/E2-3.cpp.o"
+  "OJ"
   "OJ.pdb"
-  "libOJ.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
---
+++ README.md
@@ -0,0 +1,5 @@
+# USTC-algorithm
+
+record labs and PPTs in USTC algorithm course.
+
+codes are all in `OJ` and homeworks are all in `hw`.
