9c916ce1fcc2c000d671d5a488fd6a600306a205
yrr <624451680@qq.com>
2021-11-18 23:58:18 +0800

    update EX2-EX

--- OJ/E2-EX.cpp
+++ OJ/E2-EX.cpp
@@ -1,6 +1,35 @@
-#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <set>
+#include<algorithm>
+#include <string>
 
-int main(){
+using namespace std;
+typedef struct edge {
+    int i;
+    int j;
+    int alpha;
+} edge;
+edge a[1000000];
+bool cmp(edge a, edge b);
 
+int main() {
+    int n, m;
+    int x, y, alpi;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < m; ++i) {
+        scanf("%d %d %d", &x, &y, &alpi);
+        a[i].i = x;
+        a[i].j = y;
+        a[i].alpha = alpi;
+    }
+    sort(a,a+m,cmp);
+    for (int i = 0; i < m; ++i) {
+        printf("%d ", a[i].alpha);
+    }
+}
+
+bool cmp(edge a, edge b) {
+    return a.alpha > b.alpha;
 }
 
--- OJ/cmake-build-debug/CMakeFiles/OJ.dir/cmake_clean.cmake
+++ OJ/cmake-build-debug/CMakeFiles/OJ.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/OJ.dir/E2-EX.cpp.o"
-  "OJ"
+  "CMakeFiles/OJ.dir/E2-EX.cpp.obj"
+  "OJ.exe"
+  "OJ.exe.manifest"
   "OJ.pdb"
+  "libOJ.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
