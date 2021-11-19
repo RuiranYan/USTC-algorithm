f3e41ab6ebf9471693f9f459a802cba9647419d5
RuiranYan <624451680@qq.com>
2021-11-19 12:45:37 +0800

    finish EX2-EX

--- OJ/cmake-build-debug/CMakeFiles/OJ.dir/DependInfo.cmake
+++ OJ/cmake-build-debug/CMakeFiles/OJ.dir/DependInfo.cmake
@@ -1,23 +1,15 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/62445/study/algorithm/OJ/E2-EX.cpp" "C:/Users/62445/study/algorithm/OJ/cmake-build-debug/CMakeFiles/OJ.dir/E2-EX.cpp.obj"
+  "/home/yanruiran/study/algorithm/OJ/E2-EX.cpp" "/home/yanruiran/study/algorithm/OJ/cmake-build-debug/CMakeFiles/OJ.dir/E2-EX.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  )
-
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
--- OJ/cmake-build-debug/CMakeFiles/OJ.dir/cmake_clean.cmake
+++ OJ/cmake-build-debug/CMakeFiles/OJ.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/OJ.dir/E2-EX.cpp.obj"
-  "OJ.exe"
-  "OJ.exe.manifest"
+  "CMakeFiles/OJ.dir/E2-EX.cpp.o"
+  "OJ"
   "OJ.pdb"
-  "libOJ.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
